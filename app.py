59760ec79231b338d3a9b5aaf9815642567882e2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-03 22:45:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,54 @@
-import os
-import transformers
-import pandas as pd
+"""Module app.py"""
 import gradio
+import pandas as pd
+import transformers
 
-
-examples = [
-    ['The English writer and the Afghani soldier.'],
-    ['It was written by members of the United Nation.'],
-    [('There were more than a hundred wolves in the Tiger Basin.  It is a dangerous place '
-      'after 9 p.m., especially near Lake Victoria.')]]
-
+import src.algorithms.interface
+import src.config
 
 # Pipeline
-classifier = transformers.pipeline(
-    task='ner', model=os.path.join(os.getcwd(), 'data', 'model'),
-    config=os.path.join(os.getcwd(), 'data', 'model'),
-    tokenizer=os.path.join(os.getcwd(), 'data', 'model'),
-    device='cpu')
+configurations = src.config.Config()
+
+# noinspection PyTypeChecker
+classifier = transformers.pipeline(task='ner', model=configurations.model_, device='cpu')
 
 
-def custom(text):
+def custom(piece):
     """
 
-    :param text:
+    :param piece:
     :return:
     """
 
-    tokens = classifier(text)
-    summary = pd.DataFrame.from_records(data=tokens)
-    summary = summary.copy()[['word', 'entity', 'score']]
+    tokens = classifier(piece)
 
-    return {'text': text, 'entities': tokens}, summary.to_dict(orient='records'), tokens
+    # Reconstructing & Persisting
+    tokens = tokens if len(tokens) == 0 else src.algorithms.interface.Interface().exc(
+        piece=piece, tokens=tokens)
+
+    # Summary
+    summary = pd.DataFrame.from_records(data=tokens)
+    summary = summary.copy()[['word', 'entity', 'score']] if not summary.empty else summary
+
+    return {'text': piece, 'entities': tokens}, summary.to_dict(orient='records'), tokens
 
 
 with gradio.Blocks() as demo:
 
-    gradio.Markdown(value=('<h1>Token Classification</h1><br><b>An illustration.</b>'), line_breaks=True)
+    gradio.Markdown(value='<h1>Token Classification</h1><br><b>An illustration.</b>', line_breaks=True)
 
     with gradio.Row():
         with gradio.Column(scale=3):
-            text = gradio.Textbox(label='TEXT', placeholder="Enter sentence here...", max_length=2000)
+            text = gradio.Textbox(label='TEXT', placeholder="Enter sentence here...", lines=23, max_length=4000)
         with gradio.Column(scale=2):
             detections = gradio.HighlightedText(label='DETECTIONS', interactive=False)
             scores = gradio.JSON(label='SCORES')
             compact = gradio.Textbox(label='COMPACT')
     with gradio.Row():
         detect = gradio.Button(value='Submit', variant='huggingface')
-        gradio.ClearButton([text, detections, scores, compact], variant='huggingface')
+        gradio.ClearButton([text, detections, scores, compact], variant='secondary')
 
     detect.click(custom, inputs=text, outputs=[detections, scores, compact])    
-    gradio.Examples(examples=examples, inputs=[text], examples_per_page=1)
+    gradio.Examples(examples=configurations.examples, inputs=[text], examples_per_page=1)
 
 demo.launch(server_port=7860)
